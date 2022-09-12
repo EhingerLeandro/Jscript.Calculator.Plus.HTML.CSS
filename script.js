b9019836ce251e9fdb991e6518c4fab5a9b9df87
EhingerLeandro <ehinger.leandro@hotmail.com>
2022-09-12 17:18:50 -0500

    5° commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function getHistory (){
 	return document.getElementById("history_value").innerText;
 }
+
 function printHistory(num){
 	document.getElementById("history_value").innerText=num;
 }
@@ -8,6 +9,7 @@ function printHistory(num){
 function getOutput (){
 	return document.getElementById("output_value").innerText;
 }
+
 function printOutput(num){
 	if(num==""){
 		document.getElementById("output_value").innerText=num;
@@ -18,27 +20,64 @@ function printOutput(num){
 }
 
 function getFormattedNumber(num){
+	if(num=="-"){
+		return "";
+	}
 	let n = Number(num);
 	let value = n.toLocaleString("en");
 	return value;
 }
-printOutput("9565895");
+
+printOutput("");
+
 function reverseNumberFormat(num){
 	return Number(num.replace(/,/g,''));
-
 }
-
 
 let operator = document.getElementsByClassName("operator");
 for(let i=0; i<operator.length; i++){
 	operator[i].addEventListener('click', function(){
-		alert("The operator clicked: "+this.id);
+		if(this.id=="clear"){
+			printHistory("");
+			printOutput("");
+		}
+		else if(this.id== "backspace"){
+			let output= reverseNumberFormat(getOutput()).toString();
+			if(output){
+				output = output.substring(0, output.length-1);
+				printOutput(output);
+			}
+		}
+		else{
+			let output=getOutput();
+			let history= getHistory();
+			if(output !==""){
+				output=reverseNumberFormat(output);
+				history=history+output;
+				if(this.id=="="){
+					let result= eval(history);
+					printOutput(result),
+					printHistory("");
+				}
+				else{
+					history=history+this.id;
+					printHistory(history);
+					printOutput("");
+				}
+
+			}
+		}
 	})
 }
 
 let number = document.getElementsByClassName("number");
 for(let i=0; i<number.length; i++){
 	number[i].addEventListener('click', function(){
-		alert("The operator clicked: "+this.id);
+		let output=reverseNumberFormat(getOutput());
+		if(output!=NaN){
+			output = output+this.id;
+			printOutput(output);
+		}
 	})
 }
+
